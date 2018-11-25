3acd10d1627967e1b3c923dfb9228196a5a5716a
scotronq <scotronq@umich.edu>
2018-11-25 00:19:47 -0500

    update2

--- functions/visualization/addROICircles.m
+++ functions/visualization/addROICircles.m
@@ -1,4 +1,4 @@
-function [] = addROICircles(A,circColor)
+function [] = addROICircles(A,circColor,circRadius,circLineWidth)
 %addROICircles adds circles to regions of interest
 %
 %   Input:
@@ -10,12 +10,14 @@ function [] = addROICircles(A,circColor)
 
 %% Start
 if nargin < 2;circColor = 'blue';end
+if nargin < 3;circRadius = 1.5;end
+if nargin < 4;circLineWidth = 2;end
 
 stats = regionprops('table',A,'Centroid',...
     'MajorAxisLength','MinorAxisLength');
 centers = stats.Centroid;
 diameters = mean([stats.MajorAxisLength stats.MinorAxisLength],2);
 radii = diameters/2;
-viscircles(centers,radii*1.5,'Color',circColor);
+viscircles(centers,radii*circRadius,'Color',circColor,'lineWidth',circLineWidth);
 end
 
