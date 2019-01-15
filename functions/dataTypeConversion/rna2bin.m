83ddcc72da8e2bb893b82ee1cfebb6807f472b4b
Scott (work computer) <scotronq@umich.edu>
2019-01-14 19:31:47 -0500

    gui update

--- functions/dataTypeConversion/rna2bin.m
+++ functions/dataTypeConversion/rna2bin.m
@@ -1,4 +1,4 @@
-function [rna_bin,rna_gene_names_bin]= rna2bin(rnaseq,gene_name,gene_locs,bin_size,chr_bin_lengths,method)
+function [rna_bin,rna_gene_names_bin]= rna2bin(rnaseq,geneName,geneLocs,binSize,chrBinLengths,method)
 %rna2bin converts RNA-seq data from gene to bin-level (ie 100kb, 1MB)
 %   rnaseq: rnaseq data, N x 1 vector
 %   gene_name: gene names, N x 1 vector
@@ -11,26 +11,26 @@ function [rna_bin,rna_gene_names_bin]= rna2bin(rnaseq,gene_name,gene_locs,bin_si
 
 if nargin <6; method='sum';end
 
-for chr=1:length(chr_bin_lengths)
+for chr=1:length(chrBinLengths)
     fprintf('binning chr%d \n',chr)
-    chr_idx = gene_locs(:,1)==chr;
-    rna_bin{chr,1} = zeros(chr_bin_lengths(chr),size(rnaseq,2));
+    chr_idx = geneLocs(:,1)==chr;
+    rna_bin{chr,1} = zeros(chrBinLengths(chr),size(rnaseq,2));
     
     chr_rnaseq = rnaseq(chr_idx,:);
-    chr_gene_name = gene_name(chr_idx,:);
-    chr_gene_locs = gene_locs(chr_idx,:);
+    chr_gene_name = geneName(chr_idx,:);
+    chr_gene_locs = geneLocs(chr_idx,:);
     
     %% overlap faction between gene and bin
-    overlapRatio = bboxOverlapRatio([[1:bin_size:chr_bin_lengths(chr)*bin_size]',...
-        zeros(chr_bin_lengths(chr),1), ones(chr_bin_lengths(chr),1)*bin_size,...
-        ones(chr_bin_lengths(chr),1)],...
+    overlapRatio = bboxOverlapRatio([[1:binSize:chrBinLengths(chr)*binSize]',...
+        zeros(chrBinLengths(chr),1), ones(chrBinLengths(chr),1)*binSize,...
+        ones(chrBinLengths(chr),1)],...
         [chr_gene_locs(:,2), zeros(length(chr_gene_locs),1),...
         diff(chr_gene_locs(:,2:3),1,2), ones(length(chr_gene_locs),1)],'min');
     
-    max_length_ref = max([diff(chr_gene_locs(:,2:3),1,2),ones(size(chr_gene_locs,1),1)*bin_size],[],2);
-    gene_overlap_percentages = (overlapRatio./repmat(max_length_ref',size(overlapRatio,1),1))*bin_size;
+    max_length_ref = max([diff(chr_gene_locs(:,2:3),1,2),ones(size(chr_gene_locs,1),1)*binSize],[],2);
+    gene_overlap_percentages = (overlapRatio./repmat(max_length_ref',size(overlapRatio,1),1))*binSize;
     
-    for i = 1:chr_bin_lengths(chr)
+    for i = 1:chrBinLengths(chr)
         rna_gene_names_bin{chr,1}{i,1} = chr_gene_name(logical(gene_overlap_percentages(i,:)));
     end
     
